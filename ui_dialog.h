e7909a202b7770a9fccb23858edc45ae407d5537
Punit Chaudhari <47317021+punit9@users.noreply.github.com>
2019-02-28 23:14:55 +0530

    Update ui_dialog.h

--- ui_dialog.h
+++ ui_dialog.h
@@ -5,7 +5,7 @@
 **
 ** WARNING! All changes made in this file will be lost when recompiling UI file!
 ********************************************************************************/
-
+ 
 #ifndef UI_DIALOG_H
 #define UI_DIALOG_H
 
